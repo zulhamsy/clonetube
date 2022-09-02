7aa317408e406ca4d7f7dd9445080cf0fc242b74
zulhamsy <zulham.sy@gmail.com>
2022-09-02 08:59:24 +0700

    copyright text fix

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -7,10 +7,18 @@ export default function Feed() {
     <Stack direction={{ xs: "column", md: "row" }}>
       <Box borderRight="1px solid #333">
         <Sidebar />
-        <Typography component="p" variant="caption" color="grey.600">
+        <Typography
+          component="p"
+          variant="caption"
+          color="grey.600"
+          align="center"
+          display={{ xs: "none", md: "block" }}
+        >
           Copyright 2022 - Jul
         </Typography>
       </Box>
+      {/* Actual Feed */}
+      <Box></Box>
     </Stack>
   );
 }
