f599d5fa1adad882b5e42d14c4dd40a5a0486d9d
zulhamsy <zulham.sy@gmail.com>
2022-09-05 10:34:00 +0700

    add channel preview

--- src/components/Channel.jsx
+++ src/components/Channel.jsx
@@ -1,7 +1,44 @@
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
 import React from "react";
 
 export default function Channel({ channel: { snippet } }) {
-  // const { title, thumbnails, channelTitle } = snippet;
+  const { thumbnails, channelTitle } = snippet;
 
-  return <div>Channel</div>;
+  return (
+    <Card elevation={0}>
+      <CardActionArea
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          "&.MuiCardActionArea-root:hover .MuiCardActionArea-focusHighlight": {
+            opacity: 0,
+          },
+        }}
+        onClick={() => console.log("ActionArea clicked")}
+      >
+        <CardMedia
+          component="img"
+          image={thumbnails.medium.url}
+          sx={{ borderRadius: "50%", width: "70%" }}
+        />
+        <CardContent>
+          <Typography
+            onClick={(e) => {
+              e.stopPropagation();
+              console.log("Channel clicked");
+            }}
+            variant="body2"
+          >
+            {channelTitle}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
 }
